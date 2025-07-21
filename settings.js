045d8e5736df69a05d5fc7e25cb9e470ef11dd38
savageplaysff <obizobachristian723@gmail.com>
2025-07-21 19:50:55 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: SUBZERO-MD;;;ORcmxLqK#0CTLWgGg98q6fgvzZwaHj9UtEcHHK0Nnn9tC1UVGu_c// Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
